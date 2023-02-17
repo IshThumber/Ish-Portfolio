30b6ed1eebf384f9b98be6b3844131a4892caaa9
Ish Thumber <ishthumber343@gmail.com>
2023-02-18 00:44:40 +0530

    changes

--- src/components/Main/content/Content.jsx
+++ src/components/Main/content/Content.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../header/Header";
 import Section from "../section/section";
-import Quote from '../Quotes/quote';
+import Quote from "../Quotes/quote";
 import "./Content.css";
 
 const Content = () => {
