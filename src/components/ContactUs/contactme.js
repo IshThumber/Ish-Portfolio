3aaece7980358a1f412d75998c67725a9c9c21bb
Ish Thumber <ishthumber343@gmail.com>
2023-03-28 00:24:57 +0530

    custom scroll bar

--- src/components/ContactUs/contactme.js
+++ src/components/ContactUs/contactme.js
@@ -1,7 +1,40 @@
-import React from "react";
-
+import { Box, Heading } from "@chakra-ui/react";
+// import "./contact.css";
 const Contact = () => {
-   return <div>Contact Me</div>;
+   return (
+      <>
+         <Box
+            width="60%"
+            margin="auto"
+            alignItems="center"
+            // bg="brown"
+            padding={10}
+            marginTop={10}
+         >
+            <Box
+            // bg="cyan.100"
+            >
+               <Heading
+                  as="h1"
+                  size="2xl"
+                  textAlign="center"
+                  className="about__heading"
+               >
+                  Contact Me
+               </Heading>
+            </Box>
+            <br />
+            <br />
+            <Box
+               // bg="yellow.100"
+               padding={4}
+               display="flex"
+               flexDirection="row"
+               justifyContent="space-between"
+            ></Box>
+         </Box>
+      </>
+   );
 };
 
 export default Contact;
