240036c1c070c06795787335ad5c78df1ab12395
Ish Thumber <ishthumber343@gamil.com>
2023-08-04 21:28:31 +0530

    changes1

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -17,8 +17,8 @@ const Contact = () => {
         <Heading name="#Contact Me" />
 
         <div className="flex flex-col justify-center items-center -mt-5">
-          <div className=" w-4/5 py-10 items-center sm:justify-center flex flex-col">
-            <div className="grid grid-cols-2 sm:grid-cols-3 grid-rows-2 gap-7 w-4/5 justify-center items-center">
+          <div className="w-4/5 py-10 items-center sm:justify-center flex flex-col">
+            <div className="-mt-6 grid grid-cols-2 sm:grid-cols-3 grid-rows-2 gap-7 w-4/5 justify-center items-center">
               <div className="col-span-2 sm:col-span-1">
                 <Input
                   className="shadowButton rounded-lg col-span-2 text-purple-500"
