16e5cb0c3bae6ed372adef5d2c65715f4efc2f7d
Ish Thumber <ishthumber343@gamil.com>
2024-01-16 13:43:16 +0530

    Refactor HeroMain component styling

--- src/sections/HeroMain.js
+++ src/sections/HeroMain.js
@@ -15,29 +15,30 @@ const HeroMain = () => {
   const openResume = () => {
     window.open(
       "https://drive.google.com/file/d/1dCukUpwN8EbVZwBdKMTVNDxm6M-fkbhz/view?usp=sharing",
-      "_blank"
+      "_blank",
+      "resume"
     );
   };
   return (
     <>
-      <div className="bg-transparent w-full flex flex-col justify-center items-center dark:text-wild-sand-300 transition-color duration-500">
-        <div className="items-center text-center mt-56">
-          <h3 className="font-brandonLight text-lg sm:text-2xl mb-2">
+      <div className="flex flex-col items-center justify-center w-full duration-500 bg-transparent dark:text-wild-sand-300 transition-color">
+        <div className="items-center mt-56 text-center">
+          <h3 className="mb-2 text-lg font-brandonLight sm:text-2xl">
             Hey, I&apos;m
           </h3>
-          <h1 className="font-penna font-black text-7xl sm:text-7xl md:text-9xl transition-transform transition-color duration-500">
+          <h1 className="font-black transition-transform duration-500 font-penna text-7xl sm:text-7xl md:text-9xl transition-color">
             Ish Thumber
           </h1>
         </div>
-        <div className="justify-center text-center w-4/6">
-          <p className="font-brandonLight text-sm sm:text-lg font-thin transition-color duration-500">
+        <div className="justify-center w-4/6 text-center">
+          <p className="text-sm font-thin duration-500 font-brandonLight sm:text-lg transition-color">
             I&apos;m a Full-Stack Developer who loves crafting awesome web
             experiences with ReactJS, NodeJS, and a dash of DevOps magic. Let's
             code, collaborate, and create something extraordinary together! 🚀
           </p>
         </div>
         <div className="items-center justify-center">
-          <div className="my-3 p-1 flex flex-row justify-between items-center gap-8 md:gap-20">
+          <div className="flex flex-row items-center justify-between gap-8 p-1 my-3 md:gap-20">
             <Button
               // variant="outlined"
               className="midShadowButton bg-cascade-100 hover:bg-cascade-500 text-blue-gray-800 border-gray-900 border-2 py-5 rounded-full w-[150px] md:w-[180px] dark:border-bunker-50 dark:text-wild-sand-50 dark:bg-cascade-800 dark:hover:bg-cascade-500 dark:hover:text-wild-sand-800"
@@ -56,7 +57,7 @@ const HeroMain = () => {
           </div>
         </div>
 
-        {/* <div className="w-full items-center justify-center mt-20 py-7">
+        {/* <div className="items-center justify-center w-full mt-20 py-7">
           <Quote />
         </div> */}
       </div>
